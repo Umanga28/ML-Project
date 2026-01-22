4a4fc7d0326f9ed06414fcab746f8ad0f59592a5
Umanga28 <shivapuri572@gmail.com>
2026-01-22 15:09:36 +0545

    Pipeline Prediction

---
+++ src/pipeline/predict_pipeline.py
@@ -0,0 +1,47 @@
+import os
+import sys
+import pandas as pd
+from src.exception import CustomException
+from src.utils import load_object
+
+class PredictPipeline:
+    def __init__(self):
+        pass
+    def predict(self,features):
+        try:
+            model_path="artefacts/model.pkl"
+            preprocessor_path="artefacts/preprocessor.pkl"
+            print('Before Loading')
+            model=load_object(file_path=model_path)
+            preprocessor=load_object(file_path=preprocessor_path)
+            print('After Loading')
+            data_scaled=preprocessor.transform(features)
+            preds=model.predict(data_scaled)
+            return preds
+        
+
+        except Exception as e:
+            raise CustomException(e,sys)
+class CustomData:
+    def __init__(self,gender:str,race_ethnicity:str,parental_level_of_education:str,lunch:str,test_preparation_course:str,writing_score:int,reading_score:int):
+        self.gender=gender
+        self.race_ethnicity=race_ethnicity
+        self.parental_level_of_education=parental_level_of_education
+        self.lunch=lunch
+        self.test_preparation_course=test_preparation_course
+        self.reading_score=reading_score
+        self.writing_score=writing_score
+        
+    def get_data_as_dataframe(self):
+        try:
+            custome_input_data_dict={
+                "gender":[self.gender],
+                "race_ethnicity":[self.race_ethnicity],
+                "parental_level_of_education":[self.parental_level_of_education],
+                "lunch":[self.lunch],
+                "reading_score":[self.reading_score],
+                "writing_score":[self.writing_score]
+            }
+            return pd.DataFrame(custome_input_data_dict)
+        except Exception as e:
+            raise CustomException(e,sys)
