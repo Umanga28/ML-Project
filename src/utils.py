f27d23951e1095105a708f521c26b1920e44713a
Umanga28 <shivapuri572@gmail.com>
2026-01-22 14:21:27 +0545

    Build Regressor Model

--- src/utils.py
+++ src/utils.py
@@ -23,7 +23,7 @@ def evaluate_models(X_train,y_train,X_test,y_test,models,param):
     try:
         report ={}
         for i in range(len(list(models))):
-            model=list(models.values()[i])
+            model=list(models.values())[i]
             para=param[list(models.keys())[i]]
 
             gs=GridSearchCV(model,para,cv=3)
@@ -36,7 +36,7 @@ def evaluate_models(X_train,y_train,X_test,y_test,models,param):
             # r2 score
             train_model_score=r2_score(y_train,y_train_pred)
             test_model_score=r2_score(y_test,y_test_pred)
-            report[list(model.keys())[i]]=test_model_score
+            report[list(models.keys())[i]]=test_model_score
             return report
     except Exception as e:
         raise CustomException(e,sys)
